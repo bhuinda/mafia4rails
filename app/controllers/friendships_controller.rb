2f3e0ebee12b9748ffc32862d8841d92a79d9e8c
bhuinda <bhuinda@sbcglobal.net>
2024-03-04 20:15:40 -0600

    Created Friendships controller

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,20 @@
+class FriendshipsController < ApplicationController
+  before_action :authenticate_request
+
+  # POST /friendships
+  def index
+    @friends = @current_user.friends
+    render json: @friends, status: :ok
+  end
+
+  # DELETE /friendships/1
+  def destroy
+    @friendship = Friendship.find_by(user_id: @current_user.id, friend_id: params[:id])
+
+    if @friendship.destroy
+      render json: {message: "Friendship was successfully deleted."}, status: :ok
+    else
+      render json: @friendship.errors, status: :unprocessable_entity
+    end
+  end
+end
