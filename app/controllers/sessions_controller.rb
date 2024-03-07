1c4649deeaa28dd1682ff85feac673efa54851b2
bhuinda <bhuinda@sbcglobal.net>
2024-03-06 21:37:52 -0600

    Update sessions_controller.rb
    Changed session params from username/password to email/password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by(username: params[:username])
+    user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       token = jwt_encode(user_id: user.id)
       render json: { token: token }, status: :ok     
     else
-      render json: { error: 'Invalid username or password' }, status: :unauthorized
+      render json: { error: 'Invalid email or password' }, status: :unauthorized
     end
   end
 
